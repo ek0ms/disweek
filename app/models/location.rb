a5202ac8a0b7b89f21beba7b328018069d4482f3
Jeffrey Zhen <jzhen91@gmail.com>
2016-04-28 11:13:52 -0400

    image overlay

--- app/models/location.rb
+++ app/models/location.rb
@@ -26,38 +26,40 @@ class Location < ActiveRecord::Base
   def create_photos
     response = Net::HTTP.get_response(photos_uri)
     body = JSON.parse(response.body)["data"]
-    body.each do |media|
-      if Time.now - Time.at(media["created_time"].to_i) < 604800
-        db_photo = Photo.where(link: media["link"]).first
-        if db_photo.nil?
-          Photo.create(
-            location: self,
-            link: media["link"],
-            low_res_link: media["images"]["low_resolution"]["url"],
-            created_on_insta: media["created_time"].to_i,
-            caption: media["caption"]["text"],
-            username: media["user"]["username"],
-            profile_picture: media["user"]["profile_picture"],
-            likes: media["likes"]["count"].to_i,
-            comments: media["comments"]["count"].to_i,
-            popularity: media["likes"]["count"].to_i +
-             media["comments"]["count"].to_i
-          )
-        elsif db_photo.popularity != media["likes"]["count"].to_i +
-            media["comments"]["count"].to_i || db_photo.caption !=
-                media["caption"]["text"] || db_photo.username !=
-                    media["user"]["username"] || db_photo.profile_picture !=
-                        media["user"]["profile_picture"]
-          photo = Photo.where(link: media["link"]).first
-          photo.update_attributes(
-            caption: media["caption"]["text"],
-            username: media["user"]["username"],
-            profile_picture: media["user"]["profile_picture"],
-            likes: media["likes"]["count"].to_i,
-            comments: media["comments"]["count"].to_i,
-            popularity: media["likes"]["count"].to_i +
-             media["comments"]["count"].to_i
-          )
+    if !body.empty?
+      body.each do |media|
+        if Time.now - Time.at(media["created_time"].to_i) < 604800
+          db_photo = Photo.where(link: media["link"]).first
+          if db_photo.nil?
+            Photo.create(
+              location: self,
+              link: media["link"],
+              low_res_link: media["images"]["low_resolution"]["url"],
+              created_on_insta: media["created_time"].to_i,
+              caption: media["caption"]["text"],
+              username: media["user"]["username"],
+              profile_picture: media["user"]["profile_picture"],
+              likes: media["likes"]["count"].to_i,
+              comments: media["comments"]["count"].to_i,
+              popularity: media["likes"]["count"].to_i +
+               media["comments"]["count"].to_i
+            )
+          elsif db_photo.popularity != media["likes"]["count"].to_i +
+              media["comments"]["count"].to_i || db_photo.caption !=
+                  media["caption"]["text"] || db_photo.username !=
+                      media["user"]["username"] || db_photo.profile_picture !=
+                          media["user"]["profile_picture"]
+            photo = Photo.where(link: media["link"]).first
+            photo.update_attributes(
+              caption: media["caption"]["text"],
+              username: media["user"]["username"],
+              profile_picture: media["user"]["profile_picture"],
+              likes: media["likes"]["count"].to_i,
+              comments: media["comments"]["count"].to_i,
+              popularity: media["likes"]["count"].to_i +
+               media["comments"]["count"].to_i
+            )
+          end
         end
       end
     end
